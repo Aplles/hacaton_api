00035bec745eeedff71a3571f24d659d2037925a
Maxim Karaichev <karaichevmaksim@gmail.com>
2025-09-19 22:49:26 -0800

    fix comma in installed apps

--- conf/settings/django.py
+++ conf/settings/django.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
     'service_objects',
     "corsheaders",
     "drf_spectacular",
